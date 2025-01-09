199b6d0b5b090db19f0f07129aed8c79060ed51f
range-wix <range@wix.com>
2025-01-09 11:52:58 +0200

    add md to json

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="text_format_converter",
-    version="0.2",
+    version="0.21",
     packages=find_packages(),
     install_requires=[],
     author="Ran Geler",
